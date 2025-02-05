a1428ac4540b34332fa50adb7c85115ee9bedb32
Sidhanta <sidhantanayak980@gmail.com>
2025-02-05 16:37:10 +0530

    changes in config

--- config/config.js
+++ config/config.js
@@ -9,4 +9,8 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
   },
+  production: {
+    use_env_variable: 'DATABASE_URL',  
+    dialect: 'postgres',
+  },
 };
